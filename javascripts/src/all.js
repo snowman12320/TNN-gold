6c56a309158174ddf882926f89c9448d46cc5820
william <101874444+snowman12320@users.noreply.github.com>
2022-12-09 17:06:03 +0800

    done : navbar

--- javascripts/src/all.js
+++ javascripts/src/all.js
@@ -1,1 +1,37 @@
-<script type="module" src="module.js"></script>
+//
+$(document).ready(function () {
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 20) {
+      $("#nav_topTag").css("background-color", "rgb(252, 252, 252, 0.3");
+    } else {
+      $("#nav_topTag").css("background-color", "rgb(252, 252, 252, 0)");
+    }
+  });
+});
+//
+const splide = new Splide(".splide", {
+  type: "fade", //slide, loop, fade
+  // padding: { y: 10 }, //css values
+  gap: "20px", //1 rem default
+  rewind: true,
+  speed: 1500, //transition in milliseconds
+  fixedWidth: "100vw", //"10vw",
+  width: "100vw",
+  perPage: 1, //5,
+  start: 0, //3,
+  perMove: 1, //3,
+  // autoplay: true,
+  interval: 3000, //3000,
+  arrows: false,
+  pagination: false,
+  pauseOnHover: true,
+  // wheel: true, //滑鼠控制
+  //
+  // focus: "center",
+  //   drag: "free",
+  // autoScroll: {
+  //   speed: -1,
+  // },
+  //
+});
+splide.mount();
