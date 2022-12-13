c2a533aa4a4e3ed20c69c85d8e8f20c75f060102
william <101874444+snowman12320@users.noreply.github.com>
2022-12-13 14:34:53 +0800

    done : news

--- javascripts/src/all.js
+++ javascripts/src/all.js
@@ -1,5 +1,6 @@
 //
 $(document).ready(function () {
+  //
   $(window).scroll(function () {
     if ($(this).scrollTop() > 20) {
       $(".wrap").css("background-color", "rgb(252, 252, 252, 0.8");
@@ -12,8 +13,25 @@ $(document).ready(function () {
       $(".wrap button").css("color", "rgb(255,255,255");
     }
   });
+  //
+  $(window).scroll(function () {
+    var scrollPos = $(window).scrollTop();
+    var windowHeight = $(window).height();
+    $(".animated").css("transform", "translateY(50px)");
+    // animated
+    $(".animated").each(function () {
+      var thisPos = $(this).offset().top;
+      if (windowHeight + scrollPos >= thisPos) {
+        $(this).addClass("fadeIn");
+        $(this).css("transform", "translateY(0px)");
+      }
+    });
+  });
 });
-//
+
+/**
+ *
+ */
 const splide = new Splide(".splide", {
   type: "fade", //slide, loop, fade
   // padding: { y: 10 }, //css values
@@ -25,8 +43,8 @@ const splide = new Splide(".splide", {
   perPage: 1, //5,
   start: 0, //3,
   perMove: 1, //3,
-  // autoplay: true,
-  interval: 3000, //3000,
+  autoplay: true,
+  interval: 2000, //3000,
   arrows: false,
   pagination: false,
   pauseOnHover: true,
